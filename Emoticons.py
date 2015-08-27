bcb582b5ffc3d09720fd93dcc960e5c843c72afe
Antriksh <antriksh5235@gmail.com>
2015-08-28 02:47:21 +0530

    laugh words and emotions detected

--- Emoticons.py
+++ Emoticons.py
@@ -8,10 +8,10 @@ def Emoticons():
 	print re.findall(pattern, text)
 
 def laughEmoticons():
-	eyes, noses, mouths = r":", r"-", r")DPp"
+	eyes, noses, mouths = r":%", r"-O", r")DPp"
 	pattern = "[%s][%s]?[%s]" % tuple(map(re.escape, [eyes, noses, mouths]))
-	laugh_pattern = "^.*(\b(a*ha+h[ha]*|o?l+o+l+[ol]*)\b).*?$"
+	laugh_pattern =  r"\b(a*ha+h[ha]*|o?l+o+l+[ol]*)\b"
 
-	text = "bla bla bla :-/ more text 8^P and another smiley =-D even more text lololol hahahahaha aaahahaha"
+	text = "lololol :-) hahahah aaahahahaha llloooollll"
 	print re.findall(laugh_pattern, text)
-	# print re.findall(pattern, text)
+	print re.findall(pattern, text)
